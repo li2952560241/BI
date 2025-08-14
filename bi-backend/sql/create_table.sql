ce01c24c48c818f47009c78a33317f0a46d83812
TWTW <2952560241@qq.com>
2025-08-14 15:23:07 +0800

    后端初始化完成，并且测试接口通过

--- bi-backend/sql/create_table.sql
+++ bi-backend/sql/create_table.sql
@@ -1,12 +1,11 @@
 # 数据库初始化
-# @author <a href="https://github.com/liyupi">程序员鱼皮</a>
-# @from <a href="https://yupi.icu">编程导航知识星球</a>
+
 
 -- 创建库
-create database if not exists my_db;
+create database if not exists bi_db;
 
 -- 切换库
-use my_db;
+use bi_db;
 
 -- 用户表
 create table if not exists user
