98bb77e7a24e76dace91a7af17e13150334e071b
TWTW <2952560241@qq.com>
2025-08-19 14:20:58 +0800

    异步用消息队列完成，项目教程完结。

--- yubi-frontend/config/routes.ts
+++ yubi-frontend/config/routes.ts
@@ -3,6 +3,7 @@
   { path: '/', redirect: '/add_chart' },
   { path: '/add_chart', name: '智能分析', icon: 'barChart', component: './AddChart' },
   { path: '/add_chart_async', name: '智能分析（异步）', icon: 'barChart', component: './AddChartAsync' },
+  { path: '/add_chart_async_mq', name: '智能分析（异步消息队列）', icon: 'barChart', component: './AddChartAsyncMq' },
   { path: '/my_chart', name: '我的图表', icon: 'pieChart', component: './MyChart' },
   {
     path: '/admin',
